c585323cb530536b892d3f05ddea8309cdb01254
grbcp5 <grbcp5@mst.edu>
2018-10-15 21:18:44 -0500

    Create jar

--- source/ProblemPane.java
+++ source/ProblemPane.java
@@ -12,7 +12,10 @@ package code;
 
 public class ProblemPane extends LabelPane {
 
-  public static final String PROBLEM_TEXT = "Problem Text";
+  public static final String PROBLEM_TEXT = "This exercise also extends your previous HW04." 
+    + "\nThis is a variation of ex 14.23 p.590, it is circles instead of rectangles." 
+    + "\nYou are experts on circles.  By now, everyone should be able to do it correctly." 
+    + "\nDo it right, it will be used again.";
 
   public ProblemPane() {
     super(ProblemPane.PROBLEM_TEXT);
--- source/ReferencePane.java
+++ source/ReferencePane.java
@@ -12,7 +12,8 @@ package code;
 
 public class ReferencePane extends LabelPane {
 
-  public static final String REFERENCE_TEXT = "Reference Text";
+  public static final String REFERENCE_TEXT 
+    = "Resources used include lecuture notes, the textbook, as well as Oracle Java Documentation.\n(https://docs.oracle.com/javase/8/docs/)";
 
   public ReferencePane() {
     super(ReferencePane.REFERENCE_TEXT);
